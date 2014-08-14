394cdd477d76d53bb5183e2be34a76920fa41ebd
Scott Nichols <snichols@riotgames.com>
2014-08-14 09:59:28 -0700

    base commit from http://docs.macetech.com/doku.php/led_matrix_shades

---
+++ AS1130Glasses/messages.h
@@ -0,0 +1,14 @@
+// Scrolling messages
+
+char string0[] PROGMEM = "macetech.com  ";
+char string1[] PROGMEM = "Your message here...  ";
+char string2[] PROGMEM = "Hello World!  ";
+//char string3[] PROGMEM = "  ";
+
+const char * stringArray[] PROGMEM = {
+  string0,
+  string1,
+  string2
+  //string3
+};
+
